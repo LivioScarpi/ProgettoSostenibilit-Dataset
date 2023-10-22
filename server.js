6cba741e2fb49787b046bc5516dadcb20ed61108
LivioScarpi <livio.scarpinati@gmail.com>
2023-10-22 17:13:05 +0200

    chore: added new server node and dataset dump

---
+++ server.js
@@ -0,0 +1,79 @@
+const { Client } = require('pg');
+const express = require('express');
+const cors = require('cors'); // Importa il pacchetto cors
+
+const app = express();
+const port = 3000;
+
+// Configurazione del client PostgreSQL
+const client = new Client({
+    user: 'livio',
+    host: 'localhost',
+    database: 'WorldAroundYou',
+    password: 'Stratocaster99',
+    port: 5432,
+});
+
+// Connessione al database
+client.connect()
+    .then(() => {
+        console.log('Connesso al database PostgreSQL');
+    })
+    .catch(err => {
+        console.error('Errore nella connessione al database:', err);
+    });
+
+const corsOptions = {
+    origin: 'http://localhost:8080', // Includi il tuo frontend URL qui
+};
+
+app.use(cors(corsOptions));
+app.use(express.json()); // Middleware per analizzare il corpo JSON
+
+// Esempio di endpoint per eseguire una query
+app.get('/tutti-luoghi', (req, res) => {
+    const query = 'SELECT id, place_name as value, region, lat, lon, tags FROM places;';
+    client.query(query, (err, result) => {
+        if (err) {
+            console.error('Errore nell\'esecuzione della query:', err);
+            res.status(500).json({ error: 'Errore nell\'esecuzione della query' });
+        } else {
+            res.json(result.rows);
+        }
+    });
+});
+
+app.post('/luoghi-nei-dintorni', (req, res) => {
+    const jsonBody = req.body;
+    console.log("jsonBody");
+    console.log(jsonBody);
+
+    const query = `SELECT *
+                            FROM places
+                            WHERE ST_DISTANCE(
+                                coordinates,
+                                ST_MakePoint(${jsonBody.lon}, ${jsonBody.lat})::geography
+                            ) <= 100000;`;
+
+    console.log("QUERY");
+    console.log(query);
+
+    client.query(query, (err, result) => {
+        if (err) {
+            console.error('Errore nell\'esecuzione della query:', err);
+            res.status(500).json({ error: 'Errore nell\'esecuzione della query' });
+        } else {
+            res.json(result.rows);
+        }
+    });
+});
+
+// Chiudere la connessione al database quando il server si spegne
+app.on('exit', () => {
+    client.end();
+    console.log('Connessione al database chiusa');
+});
+
+app.listen(port, () => {
+    console.log(`Il server è in ascolto sulla porta ${port}`);
+});
